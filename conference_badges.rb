0796997a43522afd0542792f3e57ea68a8d3843e
Ryan Pearce <ryan.pearce@key.me>
2020-05-24 17:02:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(speakers)
+  speakers.map do |speakers|
+    "Hello, my name is #{speakers}."
+  end
+end
+
+
+def assign_rooms(speakers)
+  speakers.each_with_index.map do |speakers, index|
+    "Hello, #{speakers}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+
+def printer(speakers)
+  batch_badge_creator(speakers).each do |badge|
+    puts badge
+end
